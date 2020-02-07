518fc13415321a3c5a9f278c0d85234b79892320
Beth Smith <bethsmith0623@gamil.com>
2020-02-07 14:11:32 -0600

    initial commit

---
+++ Basics_part_1/var_and_loop_practice.js
@@ -0,0 +1,13 @@
+// TODO: define addFavoriteBook(..) function
+
+// TODO: define printFavoriteBooks() function
+
+var favoriteBooks = [];
+
+addFavoriteBook("A Song of Ice and Fire");
+addFavoriteBook("The Great Gatsby");
+addFavoriteBook("Crime & Punishment");
+addFavoriteBook("Great Expectations");
+addFavoriteBook("You Don't Know JS");
+
+// TODO: print out favorite books
