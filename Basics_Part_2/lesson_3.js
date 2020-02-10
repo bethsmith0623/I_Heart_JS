73721f6b2ca5cca0ae7cbefdec58ca1c1dc4b361
Beth Smith <bethsmith0623@gamil.com>
2020-02-10 15:16:23 -0600

    adds Check Your Balance Flowchart problem

--- Basics_Part_2/lesson_3.js
+++ Basics_Part_2/lesson_3.js
@@ -149,3 +149,26 @@ if (room === "ballroom" && suspect === "Mr. Kalehoff") {
 if (solved === true) {
   console.log(suspect + " did it in the " + room + " with the " + weapon + "!" );
 }
+
+
+// Check Your Balance
+
+// change the values of `balance`, `checkBalance`, and `isActive` to test your code
+var balance = 325.00;
+var checkBalance = true;
+var isActive = false;
+
+// Solution
+if (checkBalance !== true) {
+  console.log("Thank you. Have a nice day!");
+} else if (isActive !== true && checkBalance === true) {
+  console.log("Your account is no longer active.");
+} else if (checkBalance === true && balance > 0) {
+  console.log("Your balance is $" + balance.toFixed(2) + ".");
+} else if (checkBalance === true && balance === 0) {
+  console.log("Your account is empty.");
+} else if (checkBalance === true && balance !== 0) {
+  console.log("Your balance is negative. Please contact bank.");  
+}
+
+
