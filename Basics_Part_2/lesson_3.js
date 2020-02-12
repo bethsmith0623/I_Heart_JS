fec948204d947267be5d5e956d51281dc3686871
Beth Smith <bethsmith0623@gamil.com>
2020-02-11 18:53:40 -0600

    adds switch statement notes and problems

--- Basics_Part_2/lesson_3.js
+++ Basics_Part_2/lesson_3.js
@@ -324,4 +324,67 @@ switch (option) {
   case 5: console.log("You selected option 5."); break;
   case 6: console.log("You selected option 6."); break;
 }
+ /* a default case can be added to a switch statement and will be
+ executed when none of the values match the value of the expression.
+ Here's an example with a default case.*/
+ var prize = "";
+ var winner = 3;
 
+ switch (winner) {
+   case 1:
+     prize += "a trip for two to the Bahamas and ";
+   case 2:
+     prize += "a four piece furniture set.";
+     break;
+   case 3:
+     prize += "a smartwatch and ";
+   default:
+     prize += "tickets to the circus.";
+ }
+ 
+ console.log("You've won " + prize);
+
+ //Back to school problem
+
+ /* In 2015, the U.S. Bureau of Labor Statistics conducted research 
+ to reveal how average salary is directly related to the number of 
+ years spent in school. In their findings, they found that people 
+ with:
+
+no high school diploma earned an average of $25,636/year,
+a high school diploma earned an average of $35,256/year,
+an Associate's degree earned an average of $41,496/year,
+a Bachelor's degree earned an average of $59,124/year,
+a Master's degree earned an average of $69,732/year,
+a Professional degree earned an average of $89,960/year,
+and a Doctoral degree earned an average of $84,396/year. 
+
+Write a switch statement to set the average salary of a person 
+based on their type of completed education.
+
+Afterwards, print the following to the console.
+
+In 2015, a person with __________ earned an average of __________
+/year.
+*/
+
+// change the value of `education` to test your code
+var education = 'no high school diploma';
+
+// set the value of this based on a person's education
+var salary = 0;
+
+switch(education) {
+  case "no high school diploma": salary = 25636; break;
+  case "a high school diploma": salary = 35256; break;
+  case "an Associate's degree": salary = 41496; break;
+  case "a Bachelor's degree": salary = 59124; break;
+  case "a Master's degree": salary = 69732; break;
+  case "a Professional degree": salary = 89960; break;
+  case "a Doctoral degree": salary = 84396; break;
+}
+
+console.log("In 2015, a person with " + education + 
+  " earned an average of $" + salary.toLocaleString("en-US") + "/year.");
+
+  
